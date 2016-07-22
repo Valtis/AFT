8a4f524cb84e20dd18ed010c634f0cdc7c197ee4
Erkka Kääriä <erkka.kaaria@intel.com>
2016-07-22 12:29:13 +0300

    AFT: Add support for GTest based tests
    AFT now supports GTest based tests. This support mostly delegates
    actual functionality to third party python scripts - AFT only invokes
    this script with the test cases specified by the test manifest.
    
    Signed-off-by: Erkka Kääriä <erkka.kaaria@intel.com>

---
+++ testcases/gtestcase.py
@@ -0,0 +1,47 @@
+# coding=utf-8
+# Copyright (c) 2013-2016 Intel, Inc.
+# Author Erkka Kääriä <erkka.kaaria@intel.com>
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; version 2 of the License
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+
+"""
+Test case class for Google Test based test binaries. Assumes that a Python script
+exists that handles both calling the actual binaries and then parsing the results.
+
+Separate script is used to increase flexibility, as AFT can remain agnostic
+to any implementation details.
+"""
+
+from aft.testcases.basictestcase import BasicTestCase
+
+class GTestCase(BasicTestCase):
+    def __init__(self, config):
+        super(GTestCase, self).__init__(config)
+        self.test_manifest = config["test_manifest"]
+
+    def run(self, device):
+        param = self.parameters
+        success = True
+
+        # test manifest contains the list of tests that should be executed.
+        with open(self.test_manifest) as manifest:
+            for line in manifest:
+                line = line.strip()
+
+                if line.startswith("#"):
+                    continue
+                self.parameters = param + " -n " + line
+                self.run_local_command()
+                success = success and self._success()
+
+        return success
+
+    def _success(self):
+        return not "FAILED" in self.output
